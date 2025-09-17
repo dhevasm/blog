5b7867f7160b4dc698f7a3c8572e5b8838365007
dhevasm <dhevasm2019@gmail.com>
2025-09-17 23:21:43 +0700

    feat: auth api login & register

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "../../../../../generated/prisma";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+export default async function POST(request: NextRequest){
+ try{
+    const data = await request.formData();
+    const email = data.get('email')?.toString();
+    if(!email) return NextResponse.json({status:"error", message: 'Email is required'}, {status: 400});
+    const password = data.get('password')?.toString();
+    if(!password) return NextResponse.json({status:"error", message: 'Password is required'}, {status: 400});
+
+    const user = await prisma.users.findUnique({ where: { email } });
+    if (!user) return NextResponse.json({ status: "error", message: 'User not found' }, { status: 404 });
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) return NextResponse.json({ status: "error", message: 'Invalid password' }, { status: 401 });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+    return NextResponse.json({ status: "success", token: token, message: 'Login successful' }, { status: 200 });
+
+ }catch(error){
+    return NextResponse.json({ status:"error", message: error}, {status: 500});
+ }  
+}
--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,16 +1,37 @@
 import { NextResponse, NextRequest } from "next/server";
+import { PrismaClient } from "../../../../../generated/prisma";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
 
 export default async function POST(request: NextRequest){
     try{
         const data = await request.formData();
         const name = data.get('name')?.toString();
-        if(!name) return NextResponse.json({message: 'Name is required'}, {status: 400});
+        if(!name) return NextResponse.json({status:"error", message: 'Name is required'}, {status: 400});
         const email = data.get('email')?.toString();
-        if(!email) return NextResponse.json({message: 'Email is required'}, {status: 400});
+        const checkEmail = await prisma.users.findUnique({where: {email}});
+        if(checkEmail) return NextResponse.json({status:"error", message: 'Email already exists'}, {status: 400});
+        if(!email) return NextResponse.json({status:"error", message: 'Email is required'}, {status: 400});
         const password = data.get('password')?.toString();
-        if(!password) return NextResponse.json({message: 'Password is required'}, {status: 400});
+        if(!password) return NextResponse.json({status:"error", message: 'Password is required'}, {status: 400});
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.users.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        });
+
+        if(!user) return NextResponse.json({status:"error", message: 'Error creating user'}, {status: 500});
+
+        return NextResponse.json({status:"success", data: user, message: 'User created successfully'}, {status: 201});
+
     }catch(error){
-        return NextResponse.json({message: 'error: '+error}, {status: 500});
+        return NextResponse.json({ status:"error", message: error}, {status: 500});
     }
 
 }
